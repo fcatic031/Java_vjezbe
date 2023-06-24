e93eed5d3201a9bf86628104660f07fce02cee61
fcatic031 <fcatic031@gmail.com>
2023-06-24 16:17:06 +0200

    Adding Fibonacci sequence

---
+++ eclipse/JavaVjezbe/src/javaVjezbe/FibonacciSequence.java
@@ -0,0 +1,19 @@
+package javaVjezbe;
+
+public class FibonacciSequence {
+	public static void main(String[] args) {
+		
+		int x1=0;
+		int x2=1;
+		int x3=0;
+		
+		for (int i=0; i<15;i++) {
+			System.out.println(x1);
+			x3=x1+x2;
+			x1=x2;
+			x2=x3;
+			
+			
+		}
+	}
+}
