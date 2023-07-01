48ae55c29b0141803426a36b9b92e891f624f9cc
fcatic031 <fcatic031@gmail.com>
2023-07-01 15:55:44 +0200

    Editing P003
    Around 45 seconds takes to run a code

--- eclipse/ProjectEuler/src/training/P003LargestPrimeFactor.java
+++ eclipse/ProjectEuler/src/training/P003LargestPrimeFactor.java
@@ -3,7 +3,7 @@ package training;
 public class P003LargestPrimeFactor {
 	public static void main(String[] args) {
 		long number=600851475143L;
-		System.out.println(LargestPrimeFactor(29032));
+		System.out.println(LargestPrimeFactor(number));
 	}
 	
 	public static long LargestPrimeFactor(long n) {
@@ -11,25 +11,33 @@ public class P003LargestPrimeFactor {
 		
 		boolean prim=true;
 		long m=0;
-		for (long i=2L; i<n ; i++) {
-			prim=true;
-			for (long j=2L; j<i; j++) {
-				if (i%j==0) {
-					prim=false;
-				}
-			}if (prim) {
-				if (n%i==0) {
-					//System.out.println(i);
-					m=i;
-				}
+		long broj=0;
+		
+		for (long i=2L;i<n;i++) {
+			if (n%i==0) {
+				m=n/i;
+				break;
 			}
-				
-				
+		}
+		
+		for (long i=2L; i<=m ; i++) {
+			if (n%i==0) {
+				//System.out.println(i);
+				for (long j=2L; j<i; j++) {
+					if (i%j==0) {
+						prim=false;
+						break;
+					}
+			}
+				if (prim) {
+					broj=i;
+				}
+
+			}
 			
 		}
 		
-		
 		System.out.print("Biggest prime factor of number " + n+ " would be: ");
-		return m;
+		return broj;
 	}
 }
