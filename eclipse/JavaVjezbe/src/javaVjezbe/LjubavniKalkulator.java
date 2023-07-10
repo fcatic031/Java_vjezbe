1a8d6b03f8ec3e1143fb69e9d09ad17b437aa619
fcatic031 <fcatic031@gmail.com>
2023-07-10 16:33:37 +0200

    Ljubavni Kalkulator poboljsan
    jos samo nedostaje sitnice oko zamjene imena, slučaj 100% te dvoznamenkasti brojevi na početnom nizu

--- eclipse/JavaVjezbe/src/javaVjezbe/LjubavniKalkulator.java
+++ eclipse/JavaVjezbe/src/javaVjezbe/LjubavniKalkulator.java
@@ -1,29 +1,20 @@
 package javaVjezbe;
 
+import java.util.Arrays;
+
 public class LjubavniKalkulator {
 	public static void main(String[] args) {
-		LjubavniPostotak("Anjaaa", "Fiiiiliip");
+		LjubavniPostotak("Anja", "Filip");
 	}
 	
 	
 	public static int LjubavniPostotak(String ime1, String ime2) {
-
-		
-		
-//		for (char en : ime1.toCharArray()) {
-//			System.out.print(en+"\t");
-//		}
-//		System.out.println();
-//		for (char en : ime2.toCharArray()) {
-//			System.out.print(en+"\t");
-//		}
 		String imena = ime1+ime2;
 		imena=imena.toLowerCase();
 		
 		System.out.println(imena);
 		
 		int[] brojevi= new int[imena.length()];
-		//int[] brojevi = {};
 		int j=0;
 		
 		for (char en : imena.toCharArray()) {
@@ -35,64 +26,60 @@ public class LjubavniKalkulator {
 			}
 			brojevi[j++]=i;
 		}
+		System.out.println(Arrays.toString(brojevi));
 		
-		for (int i : brojevi) {
-			System.out.print(i+" ");
-			
-		}
+		int[] noviPopis=new int[2];
+		noviPopis=Postupak(brojevi);
+		
+		int postotak=noviPopis[0]*10+noviPopis[1];
+		System.out.println("Ljubav izmedju osobe "+ime1+" i "+ime2+" iznosi "+ postotak+"%");
+		
+		return postotak;
+	}
+	
+	public static int[] Postupak(int[] brojevi){
 		int xmin=0;
 		int xmax=brojevi.length-1;
-		System.out.println();
-		String s="";
 		int n=0;
-		
 		while (xmax>xmin) {
 			if ((brojevi[xmin++]+brojevi[xmax--])<10) {
 				n++;
 			} else {
 				n+=2;
-			}
+				}
 			if (xmax==xmin) {
 				n++;
-			}
+				}
 		}
 		
-		int[] noviBrojevi= new int[n];
+		
+		int[] brojevi2 = new int[n];
 		xmin=0;
 		xmax=brojevi.length-1;
-		
-		
+		int i=0;
 		
 		while (xmax>xmin) {
-			System.out.print(brojevi[xmin++]+brojevi[xmax--]);
-			//String str = String.valueOf(brojevi[xmin++]+brojevi[xmax--]);
-			//s=s+" "+str;
-			System.out.print("\t");
+			int x=brojevi[xmin++]+brojevi[xmax--];
+			if (x<10) {
+				brojevi2[i++]=x;
+			} else if (x>9 && x<100) {
+				brojevi2[i++]=x/10;
+				brojevi2[i++]=x%10;
+			}
+			//brojevi2[xmin]=brojevi[xmin++]+brojevi[xmax--];
+		
 			if (xmax==xmin) {
-				System.out.print(brojevi[xmin]);
-				//str=String.valueOf(brojevi[xmin]);
-				//s=s+" "+str;
+				brojevi2[i]=brojevi[xmin];
 			}
 		}
 		
-		System.out.println();
-		System.out.println(s);
-		System.out.println(n);
+		if (n>2) {
+			System.out.println(Arrays.toString(brojevi2));
+			brojevi2=Postupak(brojevi2);
+		}
+		return brojevi2;
 		
-//		int a1=brojevi[xmin++]+brojevi[xmax--];
-//		noviBrojevi=new int[1];
-//		noviBrojevi[0]=a1;
-//		int a2=brojevi[xmin++]+brojevi[xmax--];
-//		//noviBrojevi=new int[2];
-//		int a3=brojevi[xmin++]+brojevi[xmax--];
-//		int a4=brojevi[xmin++]+brojevi[xmax--];
-//		System.out.println();
-//		System.out.println(a1+" "+a2+" "+a3+" "+a4);
-//		System.out.println(xmin+" "+xmax);
-		//paran broj -> dodavati dok xmin>xmax
-		//neparan broj -> jednaki xmin i xmax dodati taj jos
 		
-				
-		return 0;
+		
 	}
 }
