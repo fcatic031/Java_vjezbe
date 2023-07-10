7ed43f3912aee4f417489b3216eb1b673499e235
fcatic031 <fcatic031@gmail.com>
2023-07-10 17:32:07 +0200

    Ispravak ljubavnog kalkulatora
    Ispravljanje dvoznamenkastih i troznamenkastih brojeva na početku

--- eclipse/JavaVjezbe/src/javaVjezbe/LjubavniKalkulator.java
+++ eclipse/JavaVjezbe/src/javaVjezbe/LjubavniKalkulator.java
@@ -4,15 +4,13 @@ import java.util.Arrays;
 
 public class LjubavniKalkulator {
 	public static void main(String[] args) {
-		LjubavniPostotak("Anja", "Filip");
+		LjubavniPostotak("Slavko", "Severina");
 	}
 	
 	
 	public static int LjubavniPostotak(String ime1, String ime2) {
 		String imena = ime1+ime2;
 		imena=imena.toLowerCase();
-		
-		System.out.println(imena);
 		
 		int[] brojevi= new int[imena.length()];
 		int j=0;
@@ -26,10 +24,39 @@ public class LjubavniKalkulator {
 			}
 			brojevi[j++]=i;
 		}
-		System.out.println(Arrays.toString(brojevi));
+		int n=0;
+		for (int en:brojevi) {
+			if (en<10) {
+				n++;
+			} else if (en>9 && en<100) {
+				n+=2;
+			}
+			else if (en>99) {
+				n+=3;
+			}
+		}
+		int[] noviBrojevi=new int[n];
+		n=0;
+		for (int en: brojevi) {
+			if (en<10) {
+				noviBrojevi[n++]=en;
+			} else if (en>9 && en<100) {
+				noviBrojevi[n++]=en/10;
+				noviBrojevi[n++]=en%10;
+			}
+			else if (en>99) {
+				noviBrojevi[n++]=en/100;
+				noviBrojevi[n++]=(en/10)%10;
+				noviBrojevi[n++]=en%10;
+			}
+		}
+		
+		
+		
+		System.out.println(Arrays.toString(noviBrojevi));
 		
 		int[] noviPopis=new int[2];
-		noviPopis=Postupak(brojevi);
+		noviPopis=Postupak(noviBrojevi);
 		
 		int postotak=noviPopis[0]*10+noviPopis[1];
 		System.out.println("Ljubav izmedju osobe "+ime1+" i "+ime2+" iznosi "+ postotak+"%");
